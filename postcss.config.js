6c1ef13907b4a96ff48bd5cb69e80662bd84eba0
Mark Anthony Navarro <navarro.markanthony.tud@gmail.com>
2024-10-22 21:32:47 +0800

    added purgecss for future use idk, maybe when my machine slows down because of one big bullshit css file

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+        './View/**/*.cshtml',
+    ],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+        ...(process.env.NODE_ENV === 'production' ? { purgecss } : {}),
+    },
 }
