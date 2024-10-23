ca83042fa69da8f6697634fc7cb0da390cca7d77
Mark Anthony Navarro <navarro.markanthony.tud@gmail.com>
2024-10-24 01:03:18 +0800

    PMS v1 ✨✨✨ (getting ready for purging css)

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: [
-        './View/**/*.cshtml',
-    ],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 module.exports = {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
-        ...(process.env.NODE_ENV === 'production' ? { purgecss } : {}),
-    },
+    }
 }
--- purgecss.config.js
+++ purgecss.config.js
@@ -1,13 +1,13 @@
-const glob = require('glob');
 const path = require('path');
 
 module.exports = {
     content: [
-        path.join(__dirname, 'Views/**/*.cshtml'), 
-        path.join(__dirname, 'Scripts/**/*.js'),   
+        path.resolve(__dirname, 'Views/**/*.cshtml'),  
     ],
     css: [
-        path.join(__dirname, 'Content/**/*.css'),   
+        path.resolve(__dirname, 'wwwroot/site.css')   
     ],
-    output: './wwwroot/css/',
+    output: path.resolve(__dirname, 'wwwroot/css'),  
+    safelist: ['html', 'body', 'container'],         
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
 };
